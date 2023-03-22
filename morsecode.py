60d9047c63958ef77d2e47e8917339c08728a2b5
Suhas <saisuhas.1324@gmail.com>
2023-03-22 20:40:23 +0530

    this is an updated version using dict comp

--- morsecode.py
+++ morsecode.py
@@ -1,31 +1,25 @@
-from caci import code, code_inverse
-run = True
-while run:
-    print("\nHey welcome!, to Morse encoder/decoder:\n1.Text to Morse\n2.Morse to text\n")
-    choice = int(input("Enter your choice: "))
-    match choice:
-        case 1: 
+from caci import code
 
-            def morse(string):
-                str = ""
-                for letters in string:
-                    str = str + code[letters] + " "
-                return str
+code_inverse = {value: key for key, value in code.items()}
 
-            print(morse(input("enter a string: ").upper()))
+while True:
+  print("1. Text to Morse\n2. Morse to text\n")
+  y = int(input("Enter your choice: "))
 
-        case 2:
+  if y == 1:
+    def morse(string):
+      str = ""
+      for letter in string:
+        str += code[letter] + ' '
+      return str
+    print(morse(input("Enter a string: ").upper()))
 
-            x = input("Enter the morse string: ").split()
-            # print(x)
-            y = ""
-            for item in x:
-                y = y+ code_inverse[item] 
-            print(y, end="")
-            print()
-        
-    runAgain = input("Do you want to run again?(Yes/No): ").lower()
-    if runAgain == "yes":
-        run = True
-    else:
-        run = False
+  elif y == 2:
+    x = input("Enter a string: ").split()
+    for item in x:
+      print(code_inverse[item], end="")
+    print()
+
+  x = input("Do you want to run again? (Yes/No): ").lower()
+  if x != "yes":
+    break
